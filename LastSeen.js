b1451d3c5b5c2b99d20ca515018fe10891a7ed22
Grant Spencer <grantspencer95@gmail.com>
2021-03-08 21:37:02 -0500

    Added LastSeen.js

--- LastSeen.js
+++ LastSeen.js
@@ -1,4 +1,27 @@
 'use strict'
 
-import { Octokit } from "@octokit/core";
+// const fetch = require('node-fetch');
+
+async function getMostRecentRepo() {
+    const owner = 'GrantPSpencer'
+
+    let eventsresponse = await fetch('https://api.github.com/users/grantpspencer/events');
+    
+    let events = await eventsresponse.json();
+
+    let repo = events[0].repo.name;
+    let repolink = repo;
+    console.log(repo);
+    repo = String(repo).replace('GrantPSpencer/','');
+    console.log(repo);
+
+    let lastseen = document.getElementById('last-seen');
+
+    lastseen.innerHTML = `I was last seen working on <a href='https://github.com/${repolink}' >${repo}</a>`;
+
+    return repo;
+}
+
+getMostRecentRepo();
+
 
